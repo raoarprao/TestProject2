07c2996228aa4f379b48cb8057e6394b65cefeb3
raoadarsh@gmail.com <54360608+raoadarsh@users.noreply.github.com>
2020-02-08 17:55:55 +0530

    Add project files.

---
+++ TestProject2/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestProject2.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        public IEnumerable<string> GetAllProducts()
+        {
+            return new string[] { "product1", "product2" };
+        }
+
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "product";
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public void CreateProduct([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
